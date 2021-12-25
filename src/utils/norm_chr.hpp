cf40d08f3d9eae1ad0463a27f233bb1bb636051c
xuenhappy <nanhangxuen@163.com>
2021-12-25 15:24:19 +0800

    fix:fix code

--- src/utils/norm_chr.hpp
+++ src/utils/norm_chr.hpp
@@ -13,11 +13,27 @@
 #ifndef SRC_UTILS_NORM_CHR_HPP_
 #define SRC_UTILS_NORM_CHR_HPP_
 #include <map>
+#include <sstream>
 #include <string>
+
+#include "utf8.h"
+
 /**
  * @brief  字符串规范化使用的工具
  *
  */
 const std::map<std::string, std::string> _WordMap;
+
+/**
+ * @brief 对原始字符串进行字符归一化
+ *
+ * @param str
+ * @return std::string
+ */
+std::string normalizeStr(const std::string &str) {
+    std::stringstream output;
+
+    return output.str();
+}
 
 #endif  // SRC_UTILS_NORM_CHR_HPP_
