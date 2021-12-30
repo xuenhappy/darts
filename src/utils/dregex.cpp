3223230f50ad2978c28842733abf83ee14cfd2b7
xuen <xuen@mokajr.com>
2021-12-30 14:29:52 +0800

    fix:fix codes

---
+++ src/utils/dregex.cpp
@@ -0,0 +1,16 @@
+/*
+ * File: dregex.cpp
+ * Project: utils
+ * File Created: Thursday, 30th December 2021 1:53:52 pm
+ * Author: Xu En (xuen@mokar.com)
+ * -----
+ * Last Modified: Thursday, 30th December 2021 1:54:03 pm
+ * Modified By: Xu En (xuen@mokahr.com)
+ * -----
+ * Copyright 2021 - 2021 Your Company, Moka
+ */
+
+#include "dregex.hpp"
+
+
+void compile(StringIterPairs &pairs, Trie &trie) {}
--- src/utils/dregex.hpp
+++ src/utils/dregex.hpp
@@ -18,7 +18,7 @@
 #include <utility>
 #include <vector>
 
-#include "darts.pb.h"
+#include "./darts.pb.h"
 #include "utf8.hpp"
 namespace darts {
 
@@ -31,6 +31,12 @@ class StringIter {
      */
     virtual void iter(std::function<bool(const std::string &, size_t)> hit) = 0;
     virtual ~StringIter() {}
+};
+
+
+class StringIterPairs {
+    virtual void iter(std::function<void(StringIter &, int64_t)> hit) = 0;
+    virtual ~StringIterPairs() {}
 };
 
 class UTF8StrIterator : public StringIter {
@@ -62,6 +68,8 @@ class Trie {
     std::vector<std::vector<int64_t> *> V, OutPut;
     size_t MaxLen;
     std::map<std::string, int> CodeMap;
+
+    Trie() : MaxLen(0) {}
 
    private:
     int64_t getcode(const std::string &word) const {
@@ -140,29 +148,21 @@ class Trie {
     friend std::ostream &operator<<(std::ostream &out, const Trie &my) {
         darts::DRegexDat dat;
         dat.set_maxlen(my.MaxLen);
-        for (auto h : my.Check) {
-            dat.add_check(h);
-        }
-        for (auto h : my.Base) {
-            dat.add_base(h);
-        }
-        for (auto h : my.Fail) {
-            dat.add_fail(h);
-        }
-        for (auto h : my.L) {
-            dat.add_l(h);
-        }
+        dat.mutable_check()->Add(my.Check.begin(), my.Check.end());
+        dat.mutable_base()->Add(my.Base.begin(), my.Base.end());
+        dat.mutable_fail()->Add(my.Fail.begin(), my.Fail.end());
+        dat.mutable_l()->Add(my.L.begin(), my.L.end());
+
         for (auto s : my.V) {
-            auto l = dat.add_v();
-            for (auto item : *s) {
-                l->add_item(item);
-            }
+            dat.add_v()->mutable_item()->Add(s->begin(), s->end());
+        }
+        for (auto s : my.OutPut) {
+            dat.add_output()->mutable_item()->Add(s->begin(), s->end());
         }
 
         auto cmap = dat.mutable_codemap();
-        for (auto kv : my.CodeMap) {
-            (*cmap)[kv.first] = kv.second;
-        }
+        cmap->insert(my.CodeMap.begin(), my.CodeMap.end());
+
         if (!dat.SerializePartialToOstream(&out)) {
             std::cerr << "Failed to write Trie" << std::endl;
         }
@@ -176,47 +176,41 @@ class Trie {
         }
         my.MaxLen = dat.maxlen();
 
-        size_t size = dat.check_size();
-        my.Check.assign(size, 0);
-        for (size_t i = 0; i < size; i++) {
-            my.Check[i] = dat.check(i);
-        }
+        auto check = dat.check();
+        my.Check.insert(my.Check.end(), check.begin(), check.end());
 
-        size = dat.base_size();
-        my.Base.assign(size, 0);
-        for (size_t i = 0; i < size; i++) {
-            my.Base[i] = dat.base(i);
-        }
+        auto base = dat.base();
+        my.Base.insert(my.Base.end(), base.begin(), base.end());
 
-        size = dat.fail_size();
-        my.Fail.assign(size, 0);
-        for (size_t i = 0; i < size; i++) {
-            my.Fail[i] = dat.fail(i);
-        }
+        auto fail = dat.fail();
+        my.Fail.insert(my.Fail.end(), fail.begin(), fail.end());
 
-        size = dat.l_size();
-        my.L.assign(size, 0);
-        for (size_t i = 0; i < size; i++) {
-            my.L[i] = dat.l(i);
-        }
-        size = dat.v_size();
+        auto l = dat.l();
+        my.L.insert(my.L.end(), l.begin(), l.end());
+
+        auto size = dat.v_size();
         my.V.assign(size, NULL);
         for (size_t i = 0; i < size; i++) {
-            auto vlist = dat.v(i);
-            auto lz = vlist.item_size();
-            auto tmp = new std::vector<int64_t>(lz);
-            for (size_t j = 0; j < lz; i++) {
-                (*tmp)[j] = vlist.item(j);
-            }
-            my.V[i] = tmp;
+            auto vlist = dat.v(i).item();
+            my.V[i] = new std::vector<int64_t>(vlist.begin(), vlist.end());
         }
 
-
+        size = dat.output_size();
+        my.OutPut.assign(size, NULL);
+        for (size_t i = 0; i < size; i++) {
+            auto vlist = dat.output(i).item();
+            my.OutPut[i] = new std::vector<int64_t>(vlist.begin(), vlist.end());
+        }
         auto cmap = dat.codemap();
         my.CodeMap.insert(cmap.begin(), cmap.end());
         return in;
     }
 
+    /**
+     * @brief load data from a pb file
+     *
+     * @param path
+     */
     void loadPb(const std::string &path) {
         std::ifstream f_in(path.c_str(), std::ios::in | std::ios::binary);
         if (!f_in.is_open()) {
@@ -226,6 +220,11 @@ class Trie {
         f_in >> *this;
         f_in.close();
     }
+    /**
+     * @brief write this conf into a file
+     *
+     * @param path
+     */
     void writePb(const std::string &path) const {
         std::fstream f_out(path, std::ios::out | std::ios::trunc | std::ios::binary);
         if (!f_out.is_open()) {
@@ -237,6 +236,13 @@ class Trie {
     }
 };
 
+/**
+ * @brief compile a trie
+ *
+ * @param pairs
+ * @param trie
+ */
+void compile(StringIterPairs &pairs, Trie &trie);
 
 }  // namespace darts
 #endif  // SRC_UTILS_DREGEX_HPP_
