681e3f164c17febeaa7adfd8a2bd2d57fbf9bae1
xuen <xuen@mokajr.com>
2022-01-06 15:01:21 +0800

    fix:fix cmake

---
+++ cmake/FindJsoncpp.cmake
@@ -0,0 +1,13 @@
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(Jsoncpp_PKGCONF jsoncpp)
+
+find_path(Jsoncpp_INCLUDE_DIR
+  NAMES json/json.h
+  PATH_SUFFIXES jsoncpp
+  PATHS ${Jsoncpp_PKGCONF_INCLUDE_DIRS} # /usr/include/jsoncpp/json
+)
+
+find_library(Jsoncpp_LIBRARY
+  NAMES jsoncpp
+  PATHS ${Jsoncpp_PKGCONF_LIBRARY_DIRS}
+)
