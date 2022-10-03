7d71df0b55bf5feadc639b674b7707d3102a6985
xuen <xuen@mokahr.com>
2022-10-03 12:33:54 +0800

    fix:add codes

--- src/impl/networdqi.hpp
+++ src/impl/networdqi.hpp
@@ -12,24 +12,44 @@
 #ifndef SRC_IMPL_NETWORDQI_HPP_
 #define SRC_IMPL_NETWORDQI_HPP_
 #include "../core/darts.hpp"
-class KQNetMeasure {
+namespace darts {
+
+/**
+ * @brief 利用ONNX神经网络对数据进行表示与
+ *
+ */
+class OnnxPersenter : public CellPersenter {
+   private:
+    static const char* MODEL_PATH_KEY;
+    static const char* CHAR_TABLE_FILE_KEY;
+
    public:
     /**
-     * @brief 加载模型文件
+     * @brief init this
      *
-     * @param model_path
+     * @param param
      * @return int
      */
-    int loadModel(const char *model_path) { return EXIT_SUCCESS; }
+    int initalize(const std::map<std::string, std::string>& param) { return EXIT_SUCCESS; }
     /**
-     * @brief 利用神经网络计算两个词间距
+     * @brief set all word embeding
+     *
+     * @param dstSrc
+     * @param cmap
+     */
+    void embed(AtomList* dstSrc, SegPath* cmap) const {}
+    /**
+     * @brief
      *
      * @param pre
      * @param next
-     * @return double
+     * @return double must >=0
      */
-    double ranging(const darts::Word *pre, const darts::Word *next) const { return 0.0; }
+    double ranging(const Word* pre, const Word* next) const { return 0.0; }
+    ~OnnxPersenter() {}
 };
 
-
+const char* OnnxPersenter::MODEL_PATH_KEY = "model.path";
+REGISTER_Persenter(OnnxPersenter);
+}  // namespace darts
 #endif  // SRC_IMPL_NETWORDQI_HPP_
