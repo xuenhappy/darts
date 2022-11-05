45033e7b86daa507ad3efb3bb4a2f4d30597c147
xuen <nanhangxuen@163.com>
2022-11-05 19:18:36 +0800

    fix:add codes

--- python/train.py
+++ python/train.py
@@ -38,8 +38,26 @@ def charSplit(unicode_strs):
         yield "".join(chr_buffer), buf_type
 
 
+def _static_words(cfile):
+    words_freq = {}
+    with open(cfile) as fp:
+        for line in fp:
+            line = line.strip()
+            if not line:
+                continue
+            for w, wt in charSplit(line):
+                if wt != wtype.ENG:
+                    continue
+                words_freq[w] = words_freq.get(w, 0)+1
+    return words_freq
+
+
 def bpe_train(text_corpus_path, mini_freq, max_words_num, topk_percent, min_pmi, min_lw):
     """对文本预料进行新词发现，并在当前目录输出词频文件与二元语法统计词表
     算法参见 https://zhuanlan.zhihu.com/p/80385615
     """
-    pass
+    print(f"static the word freq from file {text_corpus_path}")
+    words_freq = _static_words(text_corpus_path)
+    print(f"filter the mini freq {mini_freq} words..")
+    words_freq = {k: v for k, v in words_freq.items() if v > mini_freq}
+    
