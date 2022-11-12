10099a0d0ffa4761c7f1f5347152d6ced3707f62
xuen <nanhangxuen@163.com>
2022-11-12 20:28:55 +0800

    fix:fix codes

--- src/impl/encoder.hpp
+++ src/impl/encoder.hpp
@@ -15,7 +15,29 @@
 
 #include <functional>
 #include <string>
+#include <vector>
 #include "../core/darts.hpp"
+
+void numToken(const std::string& num, int piceNum, std::vector<std::string>& ret) {
+    // token num str
+    if (num.length() < piceNum) {
+        ret.push_back(num);
+        return;
+    }
+    int sidx    = 0;
+    int leftnum = num.length() % piceNum;
+    int lenOuts = num.length();
+    if (leftnum != 0) {
+        lenOuts += 1;
+    } else {
+        leftnum = piceNum;
+    }
+    for (int i = 0; i < lenOuts; i++) {
+        int endi = piceNum * i + leftnum;
+        ret.push_back(num.substr(sidx, endi - sidx));
+        sidx = endi;
+    }
+}
 
 class WordPice {
    public:
@@ -33,18 +55,5 @@ class WordPice {
      */
     const char32_t* decode(int code) const;
 };
-
-/**
- * @brief  对文本预料进行新词发现，并在当前目录输出词频文件与二元语法统计词表
- * 算法参见 https://zhuanlan.zhihu.com/p/80385615
- * @param text_corpus_path 文本语料
- * @param mini_freq 最低的词频
- * @param max_words_num 输出的最大单词数目
- * @param topk_percent 输出前%k的最大的单词数目，这个值会与max_words_num并取最小值
- * @param min_pmi 最小的词概率墒
- * @param min_lw 最小词左右集合墒
- */
-void bpe_train(const char* text_corpus_path, const int mini_freq, const int max_words_num, const float topk_percent,
-               const float min_pmi, const float min_lw) {}
 
 #endif  //!__BPE_MODEL__H__
