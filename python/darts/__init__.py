5b3b88fccc456ff4a5056990ed34001d600ef4e7
xuen <xuen@mokahr.com>
2022-12-15 12:10:10 +0800

    fix:fix codes

--- python/darts/__init__.py
+++ python/darts/__init__.py
@@ -1,1 +1,9 @@
-from .cdarts import *
+def __bootstrap__():
+    import os
+    __py_dir = os.path.split(os.path.realpath(__file__))[0]
+    os.environ['LD_LIBRARY_PATH'] = "%s:%s" % (__py_dir, os.environ.get('LD_LIBRARY_PATH', ""))
+    os.environ['PYTHONPATH'] = "%s:%s" % (__py_dir, os.environ.get('PYTHONPATH', ""))
+    from .cdarts import *
+
+
+__bootstrap__()
