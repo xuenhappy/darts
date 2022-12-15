f7802a23ac4a8be336caa82fb791563e49f8f429
xuen <xuen@mokahr.com>
2022-12-15 14:13:35 +0800

    fix:add codes

--- python/darts/__init__.py
+++ python/darts/__init__.py
@@ -1,9 +1,2 @@
-def __bootstrap__():
-    import os
-    __py_dir = os.path.split(os.path.realpath(__file__))[0]
-    os.environ['LD_LIBRARY_PATH'] = "%s:%s" % (__py_dir, os.environ.get('LD_LIBRARY_PATH', ""))
-    os.environ['PYTHONPATH'] = "%s:%s" % (__py_dir, os.environ.get('PYTHONPATH', ""))
-    from .cdarts import *
-
-
-__bootstrap__()
+from .cdarts import (DSegment, Dregex, PyAtom, PyAtomList, PyWord, PyWordList, build_gramdict_fromfile, charDtype,
+    normalize)
